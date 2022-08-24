8181f36bf15f8e39d9bb9c415c39b3eacb1e0b95
Diane So <so.dianeyj@gmail.com>
2022-08-23 23:27:57 -0400

    "some changes":q

--- src/mock/data.js
+++ src/mock/data.js
@@ -4,24 +4,28 @@ import { nanoid } from 'nanoid';
 export const headData = {
   title: 'Diane So | Software Engineer', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  description: "Let's hang", // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: 'Hi, I\'m',
+  title: "Hi, I'm",
   name: 'Diane',
-  subtitle: 'your next Software Engineer',
+  subtitle: 'your next Tech PM',
   cta: 'Get to Know Me',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: 'Hi, my name is Diane So and I am a software engineer located in the Greater NYC area.',
-  paragraphTwo: 'My love for finding creative solutions to client issues first led me to consultancy and now to software development. While working closely with smaller businesses I was expected to wear many hats including editing websites. This led to my first fledgling attempts at working with HTML and CSS. I loved the experience of learning and working with code and once COVID forced me to come home to the US, I enrolled in a bootcamp program. I\'m no stranger to diving head first into learning new things and look forward to my continued development.',
-  paragraphThree: 'When I\'m not tackling some code or creating something fun, I\'m outside skiing/kayaking, working on a freelance project, or translating webtoons.',
-  list: 'Javascript, HTML5, CSS3, React, Redux, Ruby, Rails, Express, Mongoose, MongoDB, Git, Node, PostgreSQL, Heroku',
+  paragraphOne:
+    'Hi, my name is Diane So and I am a software engineer located in the Greater NYC area.',
+  paragraphTwo:
+    "My love for finding creative solutions to client issues first led me to consultancy and now to software development. While working closely with smaller businesses I was expected to wear many hats including editing websites. This led to my first fledgling attempts at working with HTML and CSS. I loved the experience of learning and working with code and once COVID forced me to come home to the US, I enrolled in a bootcamp program. I'm no stranger to diving head first into learning new things and look forward to my continued development.",
+  paragraphThree:
+    "When I'm not tackling some code or creating something fun, I'm outside skiing/kayaking, working on a freelance project, or translating webtoons.",
+  list:
+    'Javascript, HTML5, CSS3, React, Redux, Ruby, Rails, Express, Mongoose, MongoDB, Git, Node, PostgreSQL, Heroku',
   resume: 'https://drive.google.com/file/d/1zglGAlhJhQ2RGUzEh5JAMTZ81CJffMcO/view?usp=sharing', // if no resume, the button will not show up
 };
 
@@ -30,8 +34,9 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'project4.png',
-    title: "Haven",
-    info: 'Haven is a social platform for interior lovers. It allows users to create an account, log in and post rooms they want to showcase. Sign in with user: admin password: 123456.',
+    title: 'Haven',
+    info:
+      'Haven is a social platform for interior lovers. It allows users to create an account, log in and post rooms they want to showcase. Sign in with user: admin password: 123456.',
     info2: '',
     url: 'https://haven-p4.netlify.app/',
     repo: 'https://github.com/sirkittles/Haven', // if no repo, the button will not show up,
@@ -41,7 +46,8 @@ export const projectsData = [
     id: nanoid(),
     img: 'project3.jpg',
     title: "Sugar Mama's",
-    info: 'Sugar Mama\'s is an e-commerce store created in collaboration with a UX/UI team. Our dev team was able to accurately deliver the design and a full CRUD admin site which allows admin to add, edit, and delete products.',
+    info:
+      "Sugar Mama's is an e-commerce store created in collaboration with a UX/UI team. Our dev team was able to accurately deliver the design and a full CRUD admin site which allows admin to add, edit, and delete products.",
     info2: '',
     url: 'https://thecandyshop.netlify.app/',
     repo: 'https://github.com/sirkittles/sei-p3-candy', // if no repo, the button will not show up
@@ -51,7 +57,8 @@ export const projectsData = [
     id: nanoid(),
     img: 'project2.jpg',
     title: 'about.Face',
-    info: 'This React app allows you to add your face products and log your skincare routines to help you find the routine that works best for you.',
+    info:
+      'This React app allows you to add your face products and log your skincare routines to help you find the routine that works best for you.',
     info2: '',
     url: 'https://about-face.netlify.app',
     repo: 'https://github.com/sirkittles/About.Face', // if no repo, the button will not show up
@@ -61,7 +68,8 @@ export const projectsData = [
     id: nanoid(),
     img: 'project1.jpg',
     title: 'Ghibli Movie Finder',
-    info: 'Front-end web application which allows Ghibli lovers and newcomers alike to search for info on their favorite Ghibli titles. Utilizes the Ghibli API and the IMDB API to compile movie data and images.',
+    info:
+      'Front-end web application which allows Ghibli lovers and newcomers alike to search for info on their favorite Ghibli titles. Utilizes the Ghibli API and the IMDB API to compile movie data and images.',
     info2: '',
     url: 'http://ghibli-movie-finder.surge.sh/',
     repo: 'https://github.com/sirkittles/Ghibli-Movie-Finder', // if no repo, the button will not show up
@@ -72,7 +80,7 @@ export const projectsData = [
 // CONTACT DATA
 export const contactData = {
   cta: 'Would you like to work with me? Awesome!',
-  btn: 'Let\'s Talk',
+  btn: "Let's Talk",
   email: 'so.dianeyj@gmail.com',
 };
 
